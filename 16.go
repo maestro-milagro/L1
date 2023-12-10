5c2f10e6c246ae05fd475c2bb7eacaa4aa3b887e
Sergey Korchagin <millagros77@gmail.com>
2023-12-10 20:18:39 +0300

    init

--- 16.go
+++ 16.go
@@ -8,6 +8,8 @@ import (
 //Реализовать быструю сортировку массива (quicksort) встроенными методами языка.
 
 func quickSort(arr []int, low, high int) {
+	//Пока массив не отсортируется рекурсивно вызываем quickSort для левой и правой части массива
+	//т. к. посути первый вызов partition делит массив на 2 части
 	if low < high {
 		var pivot = partition(arr, low, high)
 		quickSort(arr, low, pivot)
@@ -15,19 +17,26 @@ func quickSort(arr []int, low, high int) {
 	}
 }
 
+// Функция для частичной сортировки и нахождения индекса элемента
+// вокруг которого будет проводиться сортировка в рекурсивных вызовах в функции quickSort
 func partition(arr []int, low, high int) int {
+	// Выбираем основное значение
 	var pivot = arr[low]
+	//Выделяем сортируемую область выбирая нижнюю и верхнюю границу
 	var i = low
 	var j = high
-
+	//Запускаем цикл выполнение которого будет значить что мы прошлись по всем элементам
 	for i < j {
+		//Если элемент слева меньше или равен основному значению пропускаем его
 		for arr[i] <= pivot && i < high {
 			i++
 		}
+		//Если элемент справа больше основного значения пропускаем его
 		for arr[j] > pivot && j > low {
 			j--
 		}
-
+		//Если границы еще не соеденились на одном элементе
+		//меняем левое и правое значение полученное в результате циклов местами
 		if i < j {
 			var temp = arr[i]
 			arr[i] = arr[j]
@@ -37,10 +46,11 @@ func partition(arr []int, low, high int) int {
 
 	arr[low] = arr[j]
 	arr[j] = pivot
-
+	//Возвращаем значение pivot для следующего рекурсивного вызова
 	return j
 }
 
+// Функция для вывода массива в консоль
 func printArray(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		fmt.Print(strconv.Itoa(arr[i]) + " ")
@@ -57,4 +67,5 @@ func main() {
 	quickSort(arr, 0, len(arr)-1)
 	fmt.Print("After Sorting: ")
 	printArray(arr)
+
 }
